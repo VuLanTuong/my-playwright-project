f121a8c281eb632e14a466b64c4e877d33fb0d97
VuLanTuong <lantuonglearning@gmail.com>
2026-02-28 15:09:02 +0700

    initial commit

---
+++ tests/the-internet/login.spec.ts
@@ -0,0 +1,14 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://the-internet.herokuapp.com/login');
+  await page.getByRole('textbox', { name: 'Username' }).fill('tomsmith');
+  await page.getByRole('textbox', { name: 'Password' }).fill('SuperSecretPassword!');
+  await page.getByRole('button', { name: ' Login' }).click();
+
+  await expect(page.locator('#flash')).toContainText('You logged into a secure area! ×');
+  await expect(page.getByRole('heading', { name: 'Welcome to the Secure Area.' })).toBeVisible();
+
+
+
+});
